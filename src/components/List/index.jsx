52e5ee06cb0d1bfc212e8b5a0d9d379f0261309c
VAPERSAN <vapersan@bk.ru>
2020-05-27 17:54:11 +0300

    [FIX/ADD] addlist popup && фикс некоторых файлов && classnames install

---
+++ src/components/List/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import Badge from '../Badge';
+
+import './List.scss';
+
+const List = ({items, isRemovable, onClick}) => {
+    return (
+        <ul onClick={onClick} className="list">
+            {items.map((item, index) => (
+                <li
+                    key={index}
+                    className={classNames(item.className, {active: item.active})}
+                >
+                    <i>{item.icon ? item.icon : <Badge color={item.color}/>}</i>
+                    <span>{item.name}</span>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default List;
